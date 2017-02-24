c17bb5fec276d56b0d4b453713974035315d2bcb
JazzBrotha <mattiasostblom@protonmail.com>
2017-02-24 13:38:04 +0100

    First version

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import Lists from './components/Lists'
+import Test from './components/Test'
+import Rock from './components/Rock'
+
+export default {
+  name: 'app',
+  components: {
+    Lists,
+    Test,
+    Rock
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
